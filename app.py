48536790b1d53f7f6405b33efdf8ffe31cb7b8aa
nnareshmgg@gmail.com <Pink2428$>
2017-02-14 14:20:35 -0800

    added hello tehre

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "no commens,   Today in " + location.get('city') + ": " + condition.get('text') + \
+    speech = "Hello There , Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
 
